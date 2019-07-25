f0a848d175867b03345729f8c53e6b6e9cbef08a
Frulwinn <frulwinn@gmail.com>
2019-07-25 15:46:51 -0500

    fixed issues

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -15,7 +15,7 @@ typedef struct Pair {
   Basic hash table
  ****/
 typedef struct BasicHashTable {
-  int capacity;
+  int capacity; //number of buckets/array size
   Pair **storage;
 } BasicHashTable;
 
@@ -70,26 +70,12 @@ unsigned int hash(char *str, int max)
  ****/
 BasicHashTable *create_hash_table(int capacity)
 {
-  BasicHashTable *ht;
-  //for case where capacity is 0 or less return NULL
-  if (capacity < 1) {
-    return NULL;
-  }
-  //mallocing space for hashtable
-  ht = malloc(sizeof(BasicHashTable));
-  if (ht == NULL) {
-    return NULL;
-  }
-
-  //mallocing space for size of storage
-  ht->storage = (Pair**)malloc(capacity * sizeof(Pair));
-  if (ht->storage == NULL) {
-    return NULL;
-  }
-  //fill block of memory with capacity
-  //use calloc instead
-  memset(ht->storage, 0, capacity * sizeof(Pair));
+  //allocate memory for the basic hash table struct
+  BasicHashTable *ht = malloc(sizeof(BasicHashTable));
+  //set initial values for capacity
   ht->capacity = capacity;
+  //allocate memory for the storage to hold the capacity number pairs(key,value)
+  ht->storage = calloc(capacity, sizeof(Pair *));
 
   return ht;
 }
@@ -103,27 +89,28 @@ BasicHashTable *create_hash_table(int capacity)
  ****/
 void hash_table_insert(BasicHashTable *ht, char *key, char *value)
 {
-  //node pointer points to Pair that has the key and value
-  Pair *node;
+  //hash the key to get an index
+  unsigned int hash_index = hash(key, ht->capacity); // capacity
 
-  if (ht == NULL) {
-    return 1;
-  }
+  //create a pair with the key/value
+  Pair *pair = create_pair(key, value);
 
-  node = malloc(sizeof(Pair));
-  if(node == NULL) {
-    return 1;
-  }
-  //copy of key and value is created
-  node->key = strdup(key);
-  node->value = strdup(value);
+  // Create a variable to hold the hash index
+  Pair *currently_stored_pair = ht->storage[hash_index];
+  //if the stored pair already, if the bucket is already full
+  if (stored_pair != NULL) {
+    //compare the keys if they don't match
+    if (strcmp (key, stored_pair->key) != 0) {
+        //print warning
+        printf("Warning: we don't have a collision system; going to overwrite previous value in hash table! \n")
+      }
+      //otherwise delete the pair that was previously in that index
+      destroy_pair(stored_pair);
 
-  //need to insert key and value
-  //not finished need to complete
-  node_handler(ht, node);
+    }
+    //insert the pair in the hashed index
+    ht->storage[index] = pair;
 
-
-  return 0;
 }
 
 /****
@@ -133,8 +120,23 @@ void hash_table_insert(BasicHashTable *ht, char *key, char *value)
  ****/
 void hash_table_remove(BasicHashTable *ht, char *key)
 {
+  //hash our key to get an index
+  int index = hash(key, ht->capacity); //capacity == how many "buckets" we have
 
+  //check if there's a valid entry in our bucket
+  //second part means if you are going through a linkedlist and get the same index
+  //different key but same index problem
+  if (ht->storage[index] == NULL || strcmp(ht->storage[index]->key, key) !=0) {
+    fprintf(stderr, "Unable to remove entry with this key: %s \n", key);
+
+
+  } else {
+
+    destroy_pair(ht->storage[index]);
+    ht->storage[index] = NULL;
+  }
 }
+
 
 /****
   Fill this in.
@@ -143,36 +145,18 @@ void hash_table_remove(BasicHashTable *ht, char *key)
  ****/
 char *hash_table_retrieve(BasicHashTable *ht, char *key)
 {
-  char *key_cp;
-  int i;
-  Pair *p;
+  //hash our key to get an index
+  int index = has(key, ht->capacity); //capacity == how many "buckets" we have
+  //checking if index is null and the key is matching
+  if (ht->storage[index] != NULL && strcmp(key, ht->storage[index]->key, key) == 0) {
 
-  if (ht == NULL) {
-    return NULL;
+    return ht->storage[index]->value;
   }
-  //creates copy for key
-  key_cp = strdup(key);
-  //hash key and hash table capacity
-  i = hash(key, ht->capacity);
-  //puts pair into hash table storage
-  p = ht->storage[i];
+  fprint(stderr, "unable to find entry with your key: %s \n", key);
+  return NULL;
+  
+}
 
-  //while pair is not empty  compary pair key and key copy 
-  while (p != NULL) {
-    if(strcmp(p->key, key_cp) == 0) {
-      break;
-    }
-    //go to next pair
-    p = p->next;
-  }
-  //free the copy
-  free(key_cp);
-
-  if(p == NULL) {
-    return NULL;
-  }
-
-  return p->value;
 
 /****
   Fill this in.
@@ -181,30 +165,17 @@ char *hash_table_retrieve(BasicHashTable *ht, char *key)
  ****/
 void destroy_hash_table(BasicHashTable *ht)
 {
-  Pair *p;
-  int i;
-
-  if (ht == NULL) {
-    return;
-  }
-
-  for (i = 0; i < ht->capacity; ++i {
-    //while hash tabel storage is not NULL traverse
-    if (ht->storage[i] != NULL) {
-      //traverse the list and free the nodes
-      while(ht->array[i] != NULL) {
-        p = ht->storage[i]->next;
-        free(ht->storage[i]->key);
-        free(ht->storage[i]->value);
-        free(ht->storage[i]);
-        //after freeing the key, value, and storage you still have the pair pointer
-        ht->storage[i] = p
+    //loop through each pair in the hash table's capacity
+    for (int i = 0; i < ht->capacity; i++) {
+      //if there is a pair, destroy it
+      if (ht->storage[i] != NULL) {
+        destroy_pair(ht->storage[i]);
       }
-      free(ht->storage[i]);
     }
-  }
-  free(ht->storage);
-  free(ht);
+    //free all storage
+    free(ht->storage);
+    //free the hash table
+    free(ht);
 }
 
 
